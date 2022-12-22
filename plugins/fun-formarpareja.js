a1de35b300dc587ed33abdd9b724776caad69f6e
SNTOficial <121203705+SNTOficial@users.noreply.github.com>
2022-12-22 12:20:44 -0300

    Update fun-formarpareja.js

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -5,7 +5,7 @@ let a = ps.getRandom()
 let b
 do b = ps.getRandom()
 while (b === a)
-m.reply(`*${toM(a)}, 𝙔𝙖 𝙚𝙨 𝙝𝙤𝙧𝙖 𝙙𝙚 𝙦𝙪𝙚 𝙩𝙚 💍 𝘾𝙖𝙨𝙚𝙨 𝙘𝙤𝙣 ${toM(b)}, 𝙇𝙞𝙣𝙙𝙖 𝙋𝙖𝙧𝙚𝙟𝙖 😉💓*
+m.reply(`*${toM(a)}, 𝒀𝒂 𝒆𝒔 𝒉𝒐𝒓𝒂 𝒅𝒆 𝒒𝒖𝒆 𝒕𝒆 💍 𝑪𝒂𝒔𝒆𝒔 𝒄𝒐𝒏 ${toM(b)}, 𝑳𝒊𝒏𝒅𝒂 𝑷𝒂𝒓𝒆𝒋𝒂 😉💓*
 
 *${toM(a)}, 𝙄𝙩'𝙨 𝙖𝙗𝙤𝙪𝙩 𝙩𝙞𝙢𝙚 𝙮𝙤𝙪 💍 𝙈𝙖𝙧𝙧𝙮 ${toM(b)}, 𝘾𝙪𝙩𝙚 𝙋𝙤𝙪𝙥𝙡𝙚 🤩💓*`, null, {
 mentions: [a, b]
